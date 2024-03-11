06df7810451a8fb7bfa459ca2ae0858b33710668
Ivan <ivanmaior2677@gmail.com>
2024-03-11 19:07:27 -0400

    create view for Building

--- building/views.py
+++ building/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from building.models import Building
+
+
+class BuildingView(viewsets.ModelViewSet):
+    queryset = Building.objects.all()
