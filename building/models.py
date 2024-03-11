86146bd42d83eff0d74060ea23c2c1a89357fc03
Ivan <ivanmaior2677@gmail.com>
2024-03-11 18:49:56 -0400

    Add fields to model Building

--- building/models.py
+++ building/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 
 class Building(models.Model):
-    pass
+    building_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    floors = models.IntegerField()
