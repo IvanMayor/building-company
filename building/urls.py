8731abf05dc5a63c4959c044653d08debda89148
Ivan <ivanmaior2677@gmail.com>
2024-03-12 17:42:12 -0400

    Add buildings to urls.py using Routers

--- building/urls.py
+++ building/urls.py
@@ -1,2 +1,12 @@
-urlpatterns = []
+from django.urls import path, include
+
+from .views import BuildingView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"buildings", BuildingView)
+
+
+urlpatterns = [path("", include(router.urls))]
+
 app_name = "building"
