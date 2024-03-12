3db712ca1b49c62a15a965322be3eee2351b0204
Ivan <ivanmaior2677@gmail.com>
2024-03-12 18:02:56 -0400

    Add rest framework auth token to installed apps

--- building_company/settings.py
+++ building_company/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken"
     "building",
 ]
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
